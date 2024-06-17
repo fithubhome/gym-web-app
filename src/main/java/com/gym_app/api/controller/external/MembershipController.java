c2a046b7beeec86e9cb72d8f24c20f6f1be995e8
Paul Albu <paul.i.albu@gmail.com>
2024-06-17 12:47:27 +0300

    membership types added

--- src/main/java/com/gym_app/api/controller/external/MembershipController.java
+++ src/main/java/com/gym_app/api/controller/external/MembershipController.java
@@ -1,14 +1,25 @@
 package com.gym_app.api.controller.external;
 
+import com.gym_app.api.dto.external.membership.MembershipTypeExternal;
+import com.gym_app.api.service.MembershipTypeService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/membership")
 public class MembershipController {
+
+    @Autowired
+    MembershipTypeService membershipTypeService;
+
     @GetMapping
     public String getProfile() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -18,4 +29,14 @@ public class MembershipController {
 
         return "membership/index";
     }
+
+    @GetMapping("/membershipTypes")
+    public ResponseEntity<List<MembershipTypeExternal>> getAllMembershipsType() {
+        try {
+            return ResponseEntity.status(200).body(membershipTypeService.getAllMembershipsType());
+        } catch (EntityNotFoundException ex) {
+            return ResponseEntity.status(404).body(null);
+        }
+    }
+
 }
---
+++ src/main/java/com/gym_app/api/service/MembershipTypeService.java
@@ -0,0 +1,61 @@
+package com.gym_app.api.service;
+
+import com.gym_app.api.dto.external.membership.MembershipTypeExternal;
+import com.gym_app.api.service.external.MembershipTypeServiceClient;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Getter
+public class MembershipTypeService {
+
+    @Autowired
+    private MembershipTypeServiceClient membershipTypeServiceClient;
+//    private List<MembershipTypeExternal> membershipTypeExternalList = new ArrayList<>();
+
+    public List<MembershipTypeExternal> getAllMembershipsType() throws EntityNotFoundException {
+        List<MembershipTypeExternal> membershipTypeExternalList = new ArrayList<>();
+        Optional<MembershipTypeExternal[]> optionalMembershipTypes = membershipTypeServiceClient.requestMembershipTypesExternal();
+
+        if (optionalMembershipTypes.isPresent()) {
+            Arrays.stream(optionalMembershipTypes.get()).forEach(
+                    mbTypeExt -> membershipTypeExternalList.add(
+                            mbTypeExt
+                    )
+            );
+            return membershipTypeExternalList;
+        }
+
+        throw new EntityNotFoundException(getClass().getName());
+
+    }
+
+//    public MembershipTypeService(MembershipTypeServiceClient membershipTypeServiceClient) {
+//        this.membershipTypeServiceClient = membershipTypeServiceClient;
+//
+//        Optional<MembershipTypeExternal[]> optionalMembershipTypes = membershipTypeServiceClient.requestMembershipTypesExternal();
+//
+//        if (optionalMembershipTypes.isPresent()) {
+//            Arrays.stream(optionalMembershipTypes.get()).forEach(
+//                    mbTypeExt -> membershipTypeExternalList.add(
+//                            mbTypeExt
+//                    )
+//            );
+//        } else {
+//            System.out.println("No membership types available.");
+//            throw new EntityNotFoundException(getClass().getName());
+//        }
+//    }
+
+
+}
+
