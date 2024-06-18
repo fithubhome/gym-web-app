74a17c13339daa58032adf243e15853fe8bc3f78
Paul Albu <paul.i.albu@gmail.com>
2024-06-18 15:05:00 +0300

    created Membership types dropdown list and add ONLY mbbtype name with thymeleaf

--- src/main/java/com/gym_app/api/controller/external/MembershipController.java
+++ src/main/java/com/gym_app/api/controller/external/MembershipController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -21,13 +22,15 @@ public class MembershipController {
     MembershipTypeService membershipTypeService;
 
     @GetMapping
-    public String getProfile() {
+    public ModelAndView getProfile() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
-            return "redirect:/auth/login";
+            return new ModelAndView("redirect:/auth/login");
         }
-
-        return "membership/index";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("membership/index");
+        modelAndView.addObject("objects", membershipTypeService.getAllMembershipsType());
+        return modelAndView;
     }
 
     @GetMapping("/membershipTypes")
