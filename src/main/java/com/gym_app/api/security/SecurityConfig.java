804083c370df111e8dbc43cc72b9954a5d9ff0c7
Paul Albu <paul.i.albu@gmail.com>
2024-06-25 20:02:10 +0300

    review secutiry for /submitMembership

--- src/main/java/com/gym_app/api/security/SecurityConfig.java
+++ src/main/java/com/gym_app/api/security/SecurityConfig.java
@@ -31,7 +31,9 @@ public class SecurityConfig {
                 .cors(cors -> cors.disable())
                 .authorizeHttpRequests()
                 .requestMatchers("/", "/auth/**").permitAll()
-                .requestMatchers("/membership/submitMembership").permitAll() // Added permissions for POST from Payment API
+
+                .requestMatchers( "POST","/membership/submitMembership").permitAll() // Added permissions for POST from Payment API
+
                 .requestMatchers("/dashboard", "/profile/**", "/bodystats/**", "/membership/**", "/activity/**").authenticated()
                 .requestMatchers("/role/**").hasRole("ADMIN")
                 .requestMatchers("/css/**", "/js/**", "/images/**", "/favicon.ico").permitAll()
